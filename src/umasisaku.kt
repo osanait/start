d7c2281846141f18dc0e5f0675a7ab254ce74a32
osanai <osniton19980608@yahoo.co.jp>
2021-03-20 23:09:39 +0900

    ウマ娘のデータベースのプログラミング途中

--- src/umasisaku.kt
+++ src/umasisaku.kt
@@ -1,22 +1,39 @@
-class Umamusume(name: String) {
-    var name = name
+class Umamusume(val name: String, var inputName : String) {
 
-    fun umadata(data: Array):
-
-
-    fun judge(avg: Int): String {
-        var result: String
-        if (name != inputName) {
-            result = "faild"
+    fun judge(data: Array): String {
+        var write: String
+        if (name == inputName) {
+            println(name)
+            println("芝")
+            println(data[1])
+            println("ダート")
+            println(data[2])
+            println("短距離")
+            println(data[3])
+            println("マイル")
+            println(data[4])
+            println("中距離")
+            println(data[5])
+            println("長距離")
+            println(data[6])
+            println("逃げ")
+            println(data[7])
+            println("先行")
+            println(data[8])
+            println("差し")
+            println(data[9])
+            println("追込")
+            println(data[10])
         }
-        return result
+        return write
     }
 }
 
 fun main(){
-    var a001=Umamusume("supe")
-    var data = arrayOf("A","G","F","C","A","A","G","A","A","C")//for文で順番に表示できるようにする
     println("ウマ娘の名前入力してください")
     val inputName= readLine()
-
-}
+    var a001=Umamusume("supe","inputName")
+    var data = arrayOf("A","G","F","C","A","A","G","A","A","C")
+    var write=a001.judge(data)
+    println(write)
+    }
