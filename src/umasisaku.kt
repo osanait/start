423c70111f6fa9ba9389aa9a4052152f97e5bdd4
osanai <osniton19980608@yahoo.co.jp>
2021-03-21 05:19:19 +0900

    なんか知らんけど関数使ってたぶんコード短くなった

--- src/umasisaku.kt
+++ src/umasisaku.kt
@@ -14,7 +14,14 @@ fun main(){
     println("名前を入力してください")
     var inputName = readLine()
     var a001=Umamusume("supe")
+    var a002=Umamusume("gorusi")
 if(inputName==a001.name){
     a001.avg("A","C")
 }
+    else if(inputName==a002.name){
+        a002.avg("A","G")
+    }
+    else{
+        println("ノットウマ娘")
+    }
 }
